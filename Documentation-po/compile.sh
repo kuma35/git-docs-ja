a09cfb8db0bdcba9cb57e6374d8a0d2fd58b50ca
kuma35 <sindobook@gmail.com>
2021-10-08 08:19:24 +0900

    for M-x compile command.
    use: sh -x ./compile.sh

--- Documentation-po/compile.sh
+++ Documentation-po/compile.sh
@@ -3,3 +3,12 @@ cd ${HOME}/work/git-docs-ja/Documentation-po
 make ja
 cd ${HOME}/work/git-docs-ja/Documentation-ja
 make info
+GIT_INFO="git.info"
+GITMAN_INFO="gitman.info"
+INFO_DIR="dir"
+if [ -e ${GIT_INFO} ]; then
+    install-info ${GIT_INFO} ${INFO_DIR}
+fi
+if [ -e ${GITMAN_INFO} ]; then
+    install-info ${GITMAN_INFO} ${INFO_DIR}
+fi
