ed1e1bdec5efff995ff8f37caec45958db53d13a
kuma35 <sindobook@gmail.com>
2021-11-23 04:13:11 +0900

    add install-webdoc.sh for github pages.
    JP:install-webdoc.shでgithub pages用htmlへコピーするようにした。

--- Documentation-po/compile.sh
+++ Documentation-po/compile.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
-cd ${HOME}/work/git-docs-ja/Documentation-sedout
+PROJ=${HOME}/work/git-docs-ja
+cd ${PROJ}/Documentation-sedout
 for dst_dir in technical RelNotes config howto
 do
     if [ ! -d ${dst_dir} ]; then
 	mkdir ${dst_dir}
     fi
 done
-cd ${HOME}/work/git-docs-ja/Documentation-po
+cd ${PROJ}/Documentation-po
 make ja
 exitcode=$?
 if [ ${exitcode} -ne 0 ]; then
     notify-send -u critical git-docs-ja "Documentation-po/Makefile エラー"
     exit ${exitcode}
 fi
-cd ${HOME}/work/git-docs-ja/Documentation-ja
+cd ${PROJ}/Documentation-ja
 make info $*
 exitcode=$?
 if [ ${exitcode} -ne 0 ]; then
     notify-send -u critical git-docs-ja "Documentation-ja/Makefile エラー"
     exit ${exitcode}
 fi
+#
 GIT_INFO="git.info"
 GITMAN_INFO="gitman.info"
 INFO_DIR="dir"
@@ -29,5 +31,10 @@ fi
 if [ -e ${GITMAN_INFO} ]; then
     install-info ${GITMAN_INFO} ${INFO_DIR}
 fi
+# for github pages
+cp -v *.info ${PROJ}/docs/info
+cp -v dir ${PROJ}/docs/info
+DIFF=diff ${PROJ}/Documentation-ja/install-webdoc.sh ${PROJ}/docs/htmldocs
+#
 notify-send -u normal git-docs-ja "compile完了。"
 
