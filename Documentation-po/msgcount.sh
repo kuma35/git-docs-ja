b712ca7ee4aab43d998f3720cec0378a3f4ee96b
kuma35 <sindobook@gmail.com>
2021-11-23 09:48:46 +0900

    change file mode, print date at msgcount.sh

--- Documentation-po/msgcount.sh
+++ Documentation-po/msgcount.sh
@@ -2,4 +2,5 @@
 pushd ${HOME}/work/git-docs-ja/Documentation-po/
 # find ./ -not -path "./RelNotes/*" -type f -name "*.po" -print | gawk -f ./po_count.awk
 ls -1 *.po howto/*.po technical/*.po config/*.po | gawk -f ./po_count.awk
+date
 popd
