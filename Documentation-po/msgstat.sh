02f75cb1a3802a2c4c00821b47dfc830d03a538a
kuma35 <sindobook@gmail.com>
2022-09-25 04:34:33 +0900

    add includes/ and mergetools/, add msgstat.sh newbie option.
    JP:
    includes と mergetools フォルダに msgcount, msgstat それぞれ対応。
    msgstat.sh (po_stat.awk) に オプション newbie (ハイフン無し) を
    付けると仕掛りではなく未着手を表示するようにした。
    
    ./msgstat.sh newbie

--- Documentation-po/msgstat.sh
+++ Documentation-po/msgstat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pushd ${HOME}/work/git-docs-ja/Documentation-po/
-ls -1 *.po howto/*.po technical/*.po config/*.po | awk -f po_stat.awk
+ls -1 *.po howto/*.po technical/*.po config/*.po includes/*.po mergetools/*.po | awk -f po_stat.awk $1
 date
 popd
