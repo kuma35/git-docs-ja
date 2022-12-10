4623da75f2b6612a87f493087f0c09f0d51e3022
kuma35 <sindobook@gmail.com>
2022-12-11 08:52:49 +0900

    add ignore-matching-lines
    JP:日付が変わる場合は Date: の欄も違って来るので
    対応した

--- Documentation-po/restore-manpages.sh
+++ Documentation-po/restore-manpages.sh
@@ -2,7 +2,7 @@
 # in sh,bash, ture is 0(zero), false is non-zero
 git ls-files -m -- "*.1" "*.5" "*.7" | while read line; do
     if test -e ${line} &&
-	    git diff --quiet -I"\.TH .*" -I".\\\"    Source: .*" HEAD -- ${line} &&
+	    git diff --quiet -I"\.TH .*" -I".\\\"    Source: .*" -I".\\\"      Date: .*" HEAD -- ${line} &&
 	    ! git diff --quiet HEAD -- ${line}
     then
 	git restore --source HEAD -- ${line}
