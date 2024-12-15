12feeaf228b78c45f28cbc37e8785b9406134c34
kuma35 <sindobook@gmail.com>
2024-12-15 14:11:51 +0900

    make clean and all rebuild translated documents.
    JP: po4a.cfg を再作成したので Documentation-ja/ を再作成

---
+++ Documentation-ja/technical/api-index.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+(
+	# c=////////////////////////////////////////////////////////////////
+	c=//
+	skel=api-index-skel.txt
+	sed -e '/^\/\/ table of contents begin/q' "$skel"
+	echo "$c"
+
+	ls api-*.txt |
+	while read filename
+	do
+		case "$filename" in
+		api-index-skel.txt | api-index.txt) continue ;;
+		esac
+		# title=$(sed -e 1q "$filename")
+		title=$(sed -e '1s/^=\+[[:blank:]]\+//' -e 'q' "$filename")
+		html=${filename%.txt}.html
+		echo "* link:$html[$title]"
+	done
+	echo "$c"
+	sed -n -e '/^\/\/ table of contents end/,$p' "$skel"
+) >api-index.txt+
+
+if test -f api-index.txt && cmp api-index.txt api-index.txt+ >/dev/null
+then
+	rm -f api-index.txt+
+else
+	mv api-index.txt+ api-index.txt
+fi
