ebdf43c83e15013616965ef2a5f23364ae5be3a4
kuma35 <sindobook@gmail.com>
2024-12-17 21:45:36 +0900

    write msgid to scratch buffer version.

--- Documentation-po/po-ediff-previous-msgid.el
+++ Documentation-po/po-ediff-previous-msgid.el
@@ -3,4 +3,6 @@
 (defun po-ediff-previous-msgid ()
   "Ediff previous msgid (marked #| ) and msgid."
   (po-find-span-of-entry)
-  (po-get-msgid))
+  (let ((msgid (po-get-msgid)))
+    (with-current-buffer "*scratch*"
+      (insert msgid))))
