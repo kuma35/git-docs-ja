f197a400e65dc7ea9e426d4600cae525d2ccb895
kuma35 <sindobook@gmail.com>
2024-12-19 06:40:00 +0900

    a bit fix for elisp coding style.

--- Documentation-po/po-ediff-previous-msgid.el
+++ Documentation-po/po-ediff-previous-msgid.el
@@ -1,24 +1,39 @@
-;; po-ediff-previous-msgid
-;; previous msgid is  #| marked )
-;;(defgroup po nil
-;;  "Major mode for editing PO files"
-;;  :group 'i18n)
+;;; po-ediff-previous-msgid.el --- ediff previous-msgid and msgid.
+;;; Author: kuma35
+;;; Created: 2024/12/18
+;;; Commentary:
+;; previous msgid is  '#|' marked in comment.
+;;; Code:
 
 (defcustom po-ediff-previous-msgid-buffer-a-name "*pepm-previous-msgid*"
-  "po-ediff-previous-msgid BUFFER A name. pepm is PoEdiffPreviousMsgid"
+  "'po-ediff-previous-msgid' BUFFER A name.  pepm is PoEdiffPreviousMsgid."
   :type 'string
   :require 'po-mode
   :group 'po)
 
 (defcustom po-ediff-previous-msgid-buffer-b-name "*pepm-now-msgid*"
-  "po-ediff-previous-msgid BUFFER A name. pepm is PoEdiffPreviousMsgid"
+  "'po-ediff-previous-msgid' BUFFER A name.  pepm is PoEdiffPreviousMsgid."
   :type 'string
   :require 'po-mode
   :group 'po)
 
 
+(defun po-extract-previous-msgid (buffer start end)
+  "Delete '#|' marker and unquote text from BUFFER START END.
+delete '^#| ' each line.  then unquote.
+return is String with property."
+  (with-temp-buffer
+    (insert-buffer-substring buffer start end)
+    (goto-char (point-min))
+    (while (re-search-forward "^#\\(~\\)?|[ \t]*" nil t)
+      (replace-match "" t t))
+    (po-extract-unquoted (current-buffer) (point-min) (point-max))
+    )
+  )
+
 (defun po-ediff-previous-msgid ()
   "Ediff previous msgid (marked #| ) and msgid."
+  (interactive)
   (po-find-span-of-entry)
   (let (
 	(oldbuf (current-buffer))
@@ -28,14 +43,30 @@
     (save-current-buffer
       (set-buffer (get-buffer-create
 		   po-ediff-previous-msgid-buffer-a-name))
+      (setq buffer-read-only nil)
       (erase-buffer)
       (dolist (region untranslated-regions)
-	(insert-buffer-substring oldbuf (car region) (cdr region))
+	(insert (po-extract-previous-msgid oldbuf (car region) (cdr region)))
 	)
+      (goto-char (point-min))
+      (push-mark (point-max) t t)
+      (setq buffer-read-only t)
       (restore-buffer-modified-p nil))
     (save-current-buffer
       (set-buffer (get-buffer-create
 		   po-ediff-previous-msgid-buffer-b-name))
+      (setq buffer-read-only nil)
       (erase-buffer)
       (insert msgid)
-      (setq buffer-read-only t))))
+      (goto-char (point-min))
+      (push-mark (point-max) t t)
+      (setq buffer-read-only t)
+      (restore-buffer-modified-p nil)))
+  (ediff-regions-wordwise
+   po-ediff-previous-msgid-buffer-a-name
+   po-ediff-previous-msgid-buffer-b-name)
+  )
+
+
+(provide 'po-ediff-previous-msgid)
+;;; po-ediff-previous-msgid.el ends here
