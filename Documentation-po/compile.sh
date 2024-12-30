07244920b54856a82a2580f5b57774d776f88115
kuma35 <sindobook@gmail.com>
2024-12-31 05:55:51 +0900

    scripts for docs/ to docs/docs-ja/
    JP: docs/docs-ja に github pages 用を構築するようにするための
    スクリプト変更・追加(docs-ja-4からのdocsフォルダ構成対応)

--- Documentation-po/compile.sh
+++ Documentation-po/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 PROJ=${HOME}/work/git-docs-ja
+BRANCH=docs-ja
 cd ${PROJ}/Documentation-sedout
 for dst_dir in technical RelNotes config howto
 do
@@ -8,7 +9,7 @@ do
     fi
 done
 cd ${PROJ}/Documentation-po
-make ja
+make ja BRANCH=${BRANCH}
 exitcode=$?
 if [ ${exitcode} -ne 0 ]; then
     notify-send -u critical git-docs-ja "Documentation-po/Makefile エラー"
@@ -22,24 +23,25 @@ if [ ${exitcode} -ne 0 ]; then
     exit ${exitcode}
 fi
 # gen file "dir" for info. and publish to docs/info/
-make -f ${PROJ}/Documentation-po/publish-info.mak
+make -f ${PROJ}/Documentation-po/publish-info.mak BRANCH=${BRANCH}
 exitcode=$?
 if [ ${exitcode} -ne 0 ]; then
     notify-send -u critical git-docs-ja "publish-info.mak エラー"
     exit ${exitcode}
 fi
-# restore htmls in Documentation-ja
+# restore htmls and manpaese in Documentation-ja
 ${PROJ}/Documentation-po/restore-htmls.sh
+${PROJ}/Documentation-po/restore-manpages.sh
 # for github pages
-DIFF=diff ${PROJ}/Documentation-po/install-webdoc-only-html.sh ${PROJ}/docs/htmldocs
-gawk -f ${PROJ}/Documentation-po/publish-index.awk TEMPLATE=${PROJ}/Documentation-po/index.html.template OUTPUT=${PROJ}/docs/index.html < ${PROJ}/GIT-VERSION-FILE
+DIFF=diff ${PROJ}/Documentation-po/install-webdoc-only-html.sh ${PROJ}/docs/${BRANCH}/htmldocs
+gawk -f ${PROJ}/Documentation-po/publish-index.awk TEMPLATE=${PROJ}/Documentation-po/index.html.template OUTPUT=${PROJ}/docs/${BRANCH}/index.html < ${PROJ}/Documentation-po/GIT-VERSION-FILE
 exitcode=$?
 if [ ${exitcode} -ne 0 ]; then
     notify-send -u critical git-docs-ja "publish-index.awk エラー"
     exit ${exitcode}
 fi
 # restore htmls in docs
-cd ${PROJ}/docs
+cd ${PROJ}/docs/${BRANCH}
 ${PROJ}/Documentation-po/restore-htmls.sh
 #
 notify-send -u normal git-docs-ja "compile完了。"
--- Documentation-po/publish-info.mak
+++ Documentation-po/publish-info.mak
@@ -1,21 +1,24 @@
+# spcify BRANCH from command line arguments:
+# i.e. make -f publish-info.maek BRANCH=${BRANCH}
 PROJ=$(HOME)/work/git-docs-ja
+PUB_INFO_DIR=$(PROJ)/docs/$(BRANCH)/info
 GIT_INFO=git.info
 GITMAN_INFO=gitman.info
 DIR_FILE=dir
 
-all: $(PROJ)/docs/info/$(GIT_INFO) $(PROJ)/docs/info/$(GITMAN_INFO) $(PROJ)/docs/info/$(DIR_FILE)
+all: $(PUB_INFO_DIR)/$(GIT_INFO) $(PUB_INFO_DIR)/$(GITMAN_INFO) $(PUB_INFO_DIR)/$(DIR_FILE)
 
 $(DIR_FILE) : $(GIT_INFO) $(GITMAN_INFO)
 	install-info --dir-file=$(DIR_FILE) --info-file=$(GIT_INFO)
 	install-info --dir-file=$(DIR_FILE) --info-file=$(GITMAN_INFO)
 
-$(PROJ)/docs/info/$(GIT_INFO) : $(GIT_INFO)
+$(PROJ)/docs/$(BRANCH)/info/$(GIT_INFO) : $(GIT_INFO)
 	cp -v $< $@
 
-$(PROJ)/docs/info/$(GITMAN_INFO) : $(GITMAN_INFO)
+$(PROJ)/docs/$(BRANCH)/info/$(GITMAN_INFO) : $(GITMAN_INFO)
 	cp -v $< $@
 
-$(PROJ)/docs/info/$(DIR_FILE) : $(DIR_FILE)
+$(PROJ)/docs/$(BRANCH)/info/$(DIR_FILE) : $(DIR_FILE)
 	cp -v $< $@
 
 .PHONY: all
